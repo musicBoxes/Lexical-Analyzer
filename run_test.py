4b2edf9bb333d90c5796430040ed8875ca4ca672
musicBoxes <11611006@mail.sustc.edu.cn>
2019-10-13 21:45:08 +0800

    Update reg in lex.l

--- run_test.py
+++ run_test.py
@@ -2,3 +2,6 @@ import os
 for i in range(1, 11):
 	print(i)
 	os.system("./syn.out < ./test/test_1_r%02d.spl > ./test_result/test_1_r%02d.out"%(i, i))
+	val = os.system("diff ./test_result/test_1_r%02d.out ./test/test_1_r%02d.out > tmp.txt"%(i, i))
+	if (val == 0):
+		print("Pass!")
